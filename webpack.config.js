8fa9cec3c5853ff61aad15d4f85ad77781f9ac25
ecuster <951092973@qq.com>
2017-03-09 00:35:21 +0800

    start study webgl

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,9 @@ var config = require("./output.config");
 module.exports = {
     entry:entry,
     output:{
-        filename:"[name].js",
-        path:path.resolve(__dirname,"dist",config.name,"assets"),
-        publicPath:"/"+config.name+"/assets/"
+        filename:"assets/[name].bundle.js",  //这里可以改成assets/[name].js从而达到html和js分开的目的，当然HtmlWebpackPlugin里的filename ../要去掉
+        path:path.resolve(__dirname,"dist"),
+        publicPath:"/"
     },
     devtool:"inline-source-map",
     module:{
@@ -25,32 +25,41 @@ module.exports = {
         }]
     },
     plugins:[
-        new webpack.optimize.UglifyJsPlugin({
-            compress:{
-                warnings:false
-            }
-        }),
-        new ExtractTextPlugin('[name].css'),
+        // new webpack.optimize.UglifyJsPlugin({   //開發過程不建議使用
+        //     compress:{
+        //         warnings:false
+        //     }
+        // }),
+        new ExtractTextPlugin('assets/[name].css'),
         new HtmlWebpackPlugin({
-            title:config.name,
-            filename:"../"+config.name+".html",
-            template:"./app/"+config.name+"/"+config.name+".html",
+            filename:"demo0.html",
+            template:"./app/demo0/demo0.html",
+            chunks:["demo0"],
+            inject:true
+        }),
+        new HtmlWebpackPlugin({
+            filename:"demo1.html",
+            template:"./app/demo1/demo1.html",
+            chunks:["demo1"],
             inject:true
         }),
         new webpack.optimize.CommonsChunkPlugin({
-            name:config.name,
-            filename:config.name+".js",
-            minChunks:2
+            name:"common",
+            filename:"assets/common.js",
+            minChunks:Infinity
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        // new webapck.ProvidePlugin({
+        //     $:'jquery'
+        // })
     ],
     devServer:{
         contentBase:path.join(__dirname,"dist"),
-        publicPath:"/"+config.name+"/assets/",
+        publicPath:"/",
         historyApiFallback:true,
         hot:true,
         inline:true,
-        compress:true,
+        // compress:true,
         noInfo:true,
         port:9000,
         setup:function (app) {
--- webpack.entry.js
+++ webpack.entry.js
@@ -1,5 +1,6 @@
 var entry = {
-    demo0:"./app/demo0/main.js"
+    demo0:"./app/demo0/demo0.js",
+    demo1:"./app/demo1/demo1.js"
 
 }
 
